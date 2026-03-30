ec7bfcceb4408ea65c542d9c6b369a01ec9cfe02
Hayden Sandau <hayden1sand@gmail.com>
2026-03-30 14:53:35 -0600

    Register page methods and basic structure

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -17,8 +17,8 @@ const { rmSync } = require('fs');
 // create `ExpressHandlebars` instance and configure the layouts and partials dir.
 const hbs = handlebars.create({
   extname: 'hbs',
-  layoutsDir: __dirname + '/Views/layouts',
-  partialsDir: __dirname + '/Views/partials',
+  layoutsDir: __dirname + '/views/layouts',
+  partialsDir: __dirname + '/views/partials',
 });
 
 // database configuration
@@ -65,9 +65,70 @@ app.use(
   })
 );
 
+// Custom handlebars helpers
+Handlebars.registerHelper('isEqual', (value1, value2) => {
+    return value1 == value2;
+});
+
 // API Routes ------------------------------------------------------------------------------------------
 
+app.get("/", (req, res) => {
+    res.redirect("/register"); // TODO switch this to something else probably
+});
 
+// For getting the style.css file and other stuff in resources
+app.get("/resources/:filename", (req, res) => {
+    res.sendFile(__dirname + "/resources/" + req.params.filename);
+    // TODO Make this more secure
+});
+
+// API Routes for Registering
+
+app.get("/register", async (req, res) => {
+    res.render("pages/register");
+});
+
+app.post("/register", async (req, res) => {
+    // TODO Add password reentry
+    const username = req.body.username;
+    const password = req.body.password;
+    const passwordHashed = await bcrypt.hash(req.body.password, 10); // I dont know what the 10 means
+
+    const usernameRegex = /^[A-Za-z0-9_.-]{6,50}$/; // Username allows letters, numbers, and characters _ . - (6-50 characters total)
+    const passwordRegex = /^[^\s';]{6,50}$/; // Password allows any character excluding whitespace, ', and ; (6-50 characters total))
+
+    if (! usernameRegex.test(username) || ! passwordRegex.test(password)) { // Invalid username or password
+        res.render("pages/register", {messageType: 'warning', messageText: 'Invalid username or password'});
+        return;
+    }
+
+    const FIND_USERNAME_QUERY = "SELECT * FROM users WHERE username = $1 LIMIT 1;";
+    const INSERT_USER_QUERY = "INSERT INTO users (username, password) VALUES ($1, $2);";
+
+    db.task(async () => {
+        let sameUsername = await db.any(FIND_USERNAME_QUERY, username);
+        if (sameUsername.length > 0) {
+            res.render("pages/register", {messageType: 'warning', messageText: 'Username is already in use, please choose a different username'});
+            return;
+        }
+
+        await db.none(INSERT_USER_QUERY, [username, passwordHashed]);
+        // TODO Call login post method with username and password information instead of going to dashboard
+        res.redirect("/dashboard");
+
+    }).catch(err => {
+        console.log("ERROR: An error occurred when trying to create an account:");
+        console.log(err);
+        res.render("pages/register", {messageType: 'error', messageText: 'An error has occurred, please try again later'});
+    });
+    
+});
+
+// API Routes for Dashboard
+
+app.get("/dashboard", (req, res) => {
+    res.render("pages/dashboard");
+});
 
 // Start server and keep it listening ------------------------------------------------------------------
 app.listen(3000);
